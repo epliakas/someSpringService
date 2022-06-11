6acdc98f15b79fe3aa42021f2291d5d6fd20209a
Elena Pliakas <elena.pliakas@teampicnic.com>
2022-06-11 14:36:06 +0200

    Initial Commit

---
+++ src/main/java/com/elenappli/somespringservice/SomeSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elenappli.somespringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeSpringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elenappli/somespringservice/SomeSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elenappli.somespringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
